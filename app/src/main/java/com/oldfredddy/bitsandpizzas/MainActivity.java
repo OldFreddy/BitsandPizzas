02aea3b6a11b4da1c796a3d681f4083cd1750d4e
Freddy <goblinblin@mail.ru>
2020-05-25 14:21:15 +0300

    Меню + кнопка перехода на активность

--- app/src/main/java/com/oldfredddy/bitsandpizzas/MainActivity.java
+++ app/src/main/java/com/oldfredddy/bitsandpizzas/MainActivity.java
@@ -1,11 +1,36 @@
 package com.oldfredddy.bitsandpizzas;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //Заполнение меню; элементы добавляются на панель приложения
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_create_order:
+                //Код, выполняемый при выборе элемента Create Order
+                Intent intent = new Intent(this, OrderActivity.class);
+                startActivity(intent);
+                return true; //сообщает Андроиду, что щелчок на элементе обработан
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +39,8 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+
 
     }
 }
