6ca14feaf48e19b47b2eee9b6950c11611be3de1
Freddy <goblinblin@mail.ru>
2020-05-27 17:17:44 +0300

    Провайдер передачи информации

--- app/src/main/java/com/oldfredddy/bitsandpizzas/MainActivity.java
+++ app/src/main/java/com/oldfredddy/bitsandpizzas/MainActivity.java
@@ -2,7 +2,9 @@ package com.oldfredddy.bitsandpizzas;
 
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.ShareActionProvider;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.MenuItemCompat;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -11,16 +13,28 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ShareActionProvider shareActionProvider;
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         //Заполнение меню; элементы добавляются на панель приложения
         getMenuInflater().inflate(R.menu.menu_main, menu);
+        MenuItem menuItem = menu.findItem(R.id.action_share);
+        shareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);  //Получить ссылку на провайдера действия передачи информации и присвоить ее переменной, затем вызвать метод
+        setShareActionIntent("Want to join me for pizza?");
         return super.onCreateOptionsMenu(menu);
+    }
+
+    private void setShareActionIntent(String text) {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT, text);
+        shareActionProvider.setShareIntent(intent);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()){
+        switch (item.getItemId()) {
             case R.id.action_create_order:
                 //Код, выполняемый при выборе элемента Create Order
                 Intent intent = new Intent(this, OrderActivity.class);
@@ -39,7 +53,6 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
 
 
     }
