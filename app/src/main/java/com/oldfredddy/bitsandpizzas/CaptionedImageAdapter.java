0ccf6fa1a65e0e4c3de10e27b423ed6858def5c5
Freddy <goblinblin@mail.ru>
2020-06-22 16:25:11 +0300

    RecycleView GridLayoutManager

--- app/src/main/java/com/oldfredddy/bitsandpizzas/CaptionedImageAdapter.java
+++ app/src/main/java/com/oldfredddy/bitsandpizzas/CaptionedImageAdapter.java
@@ -1,8 +1,8 @@
 package com.oldfredddy.bitsandpizzas;
 
 import android.graphics.drawable.Drawable;
-import android.view.ContextMenu;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -12,46 +12,54 @@ import androidx.cardview.widget.CardView;
 import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
-class CaptionedImageAdapter extends RecyclerView.Adapter<CaptionedImageAdapter.ViewHolder> {
+ class CaptionedImageAdapter extends
+        RecyclerView.Adapter<CaptionedImageAdapter.ViewHolder> {
 
-    private String[] captions;
-    private int[] imageIds;
+     private String[] captions;
+     private int[] imageIds;
 
-    public CaptionedImageAdapter(String[] caption, int[] imageIds) {
-        this.captions = caption;
-        this.imageIds = imageIds;
-    }
+     public static class ViewHolder extends RecyclerView.ViewHolder{
+
+         private CardView cardView;
+
+         public ViewHolder(CardView v) {
+             super(v);
+             this.cardView = v;
+         }
+     }
+
+     public CaptionedImageAdapter(String[] captions, int[] imageIds) {
+         this.captions = captions;
+         this.imageIds = imageIds;
+     }
 
 
-    @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        CardView cv = (CardView) LayoutInflater.from(parent.getContext()).inflate(R.layout.card_captioned_image, parent,false);
-        return new ViewHolder(cv);
-    }
+     @Override
+     public int getItemCount() {
+         return captions.length;
+     }
 
-    @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
-        CardView cardView = holder.cardView;
-        ImageView imageView = (ImageView)cardView.findViewById(R.id.info_image);
-        Drawable drawable = ContextCompat.getDrawable(cardView.getContext(), imageIds[position]);
-        imageView.setImageDrawable(drawable);
-        imageView.setContentDescription(captions[position]);
-        TextView textView = (TextView)cardView.findViewById(R.id.info_text);
-        textView.setText(captions[position]);
-    }
 
-    @Override
-    public int getItemCount() {
-        return captions.length;
-    }
 
-    public static class ViewHolder extends RecyclerView.ViewHolder {
+     @Override
+     public CaptionedImageAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+         CardView cv = (CardView) LayoutInflater.from(parent.getContext()).inflate(R.layout.card_captioned_image,parent,false);
+         return new ViewHolder(cv);
+     }
 
-        private CardView cardView;
+     @Override
+     public void onBindViewHolder(ViewHolder holder, int position) {
+         CardView cardView = holder.cardView;
+         ImageView imageView = (ImageView)cardView.findViewById(R.id.info_image);
+         Drawable drawable = ContextCompat.getDrawable(cardView.getContext(), imageIds[position]);
+         imageView.setImageDrawable(drawable);
+         imageView.setContentDescription(captions[position]);
+         TextView textView = (TextView)cardView.findViewById(R.id.info_text);
+         textView.setText(captions[position]);
+     }
 
-        public ViewHolder(CardView v) {
-            super(v);
-            cardView = v;
-        }
-    }
+
+
+
+
 }
